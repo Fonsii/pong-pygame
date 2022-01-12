71a93c27eb4798829d096b9d65a1084b993719c7
Fonsii <lualfonso14200174@gmail.com>
2022-01-12 11:10:45 -0600

    Simple pong game

---
+++ Ball.py
@@ -0,0 +1,46 @@
+import pygame, random, math
+
+class Ball():
+    def __init__(self):
+        self.surfaceBall = pygame.image.load('resources/ball.png').convert()
+        sizeScreen =  pygame.display.get_surface().get_size()
+        self.rect = self.surfaceBall.get_rect()
+        self.position =  self.rect.move(sizeScreen[0]/2,sizeScreen[1]/2)
+        self.speed = 3
+        angle = 360 * random.uniform(0.1, 0.9)
+        self.xSpeed = int(self.speed * math.sin(angle))
+        if self.xSpeed == 0:
+            self.xSpeed = 1
+        self.ySpeed = int(self.speed * math.cos(angle))
+        print(self.xSpeed)
+        print(self.ySpeed)
+    
+
+    def draw(self, screen):
+        screen.blit(self.surfaceBall, self.position)
+
+
+    def doMove(self, rackets):
+        self.position.x += self.xSpeed
+        self.position.y += self.ySpeed
+        if not self.checkCollision(rackets):
+            return False, self.checkBounce()
+        return True, False
+
+
+    def checkBounce(self):
+        sizeScreen =  pygame.display.get_surface().get_size()
+        if self.position.x >= sizeScreen[0] or self.position.x <= 0:
+            return True
+        if self.position.y >= sizeScreen[1] or self.position.y <= 0:
+            self.ySpeed *= -1
+        return False
+
+
+    def checkCollision(self, rackets):
+        for racket in rackets:
+            if self.position.colliderect(racket.position):
+                self.xSpeed *= -1
+                print('collision')
+                return True
+        return False
---
+++ Racket.py
@@ -0,0 +1,45 @@
+import pygame
+
+class Racket():
+    def __init__(self, postionXStarter:int, positionYStarter:int):
+        self.surfaceRacket = pygame.image.load('resources/racket.png').convert()
+        self.rect = self.surfaceRacket.get_rect()
+        self.position =  self.rect.move(postionXStarter,positionYStarter)
+        self.speed = 5
+        self.flagsMove = [False, False]
+        self.score = 0
+
+
+    def draw(self, screen):
+        screen.blit(self.surfaceRacket, self.position)
+
+
+    def move(self, positionX, positionY):
+        self.postion.x = positionX
+        self.postion.y = positionY
+    
+
+    def doMove(self):
+        maxPosition =  pygame.display.get_surface().get_size()[1] - self.surfaceRacket.get_size()[1] - 2
+        if self.flagsMove[0]:
+            self.moveDown(maxPosition)
+        if self.flagsMove[1]:
+            self.moveUp(2)
+            
+
+    def moveDown(self, maxY):
+        if self.position.y < maxY:
+            self.position.y = self.position.y + self.speed
+
+    
+    def moveUp(self, minY):
+        if self.position.y > minY:
+            self.position.y = self.position.y - self.speed
+
+    
+    def hold(self, flag):
+        self.flagsMove[flag] = True
+
+    
+    def release(self, flag):
+        self.flagsMove[flag] = False
